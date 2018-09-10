2e9fdeb5d158b546c2e5376757d7bcebd591be78
Mateo Balcorta <misterrattail@gmail.com>
2018-09-10 15:47:07 -0700

    Add content for initiating call and post call.

--- js/containers/App.jsx
+++ js/containers/App.jsx
@@ -16,7 +16,11 @@ class App extends Component {
         disclaimer: null,
         congress: null,
         info: null,
-        tweet: null
+        tweet: null,
+        callHeader: null,
+        callSubHeader: null,
+        afterCallHeader: null,
+        afterCallContent: null
       }
     }
   }
@@ -42,7 +46,11 @@ class App extends Component {
           disclaimer: data.blocks[2].value,
           congress: data.blocks[3].value,
           info: data.blocks[4].value,
-          tweet: data.blocks[5].value
+          tweet: data.blocks[5].value,
+          callHeader: data.blocks[6].value,
+          callSubHeader: data.blocks[7].value,
+          afterCallHeader: data.blocks[8].value,
+          afterCallContent: data.blocks[9].value
         },
         loading: false
       }, () => {console.log(this.state)});
@@ -51,11 +59,18 @@ class App extends Component {
 
   render() {
     const {loading} = this.state;
-    const {homeHeader, subHeader, disclaimer, congress, info, tweet} = this.state.textContent;
+    const {homeHeader, subHeader, disclaimer, congress, info, tweet, callHeader, callSubHeader, afterCallContent, afterCallHeader} = this.state.textContent;
 
       return (
         <div style={{display: loading ? 'none' : 'block'}}>
-          <Main header={homeHeader} subHeader={subHeader} disclaimer={disclaimer} congress={congress} info={info} tweet={tweet}/>
+          <Main header={homeHeader} 
+                subHeader={subHeader} 
+                disclaimer={disclaimer} 
+                congress={congress} info={info} 
+                tweet={tweet} callHeader={callHeader} 
+                callSubHeader={callSubHeader}
+                afterCallContent={afterCallContent}
+                afterCallHeader={afterCallHeader}/>
           <Footer tweet={tweet}/>
         </div>
       )
