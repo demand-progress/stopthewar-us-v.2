723f9e8523b1b2ad8272860b6ff8e69e1075eea1
Mateo Balcorta <misterrattail@gmail.com>
2018-09-10 13:27:39 -0700

    Update loader for newer version of webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = [{
         loaders: [{
             test: /\.jsx?$/,
             exclude: /(node_modules)/,
-            loader: 'babel',
+            loader: 'babel-loader',
             query: {
                 presets: ['latest', 'react']
             }
