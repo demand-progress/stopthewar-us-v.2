0dbc0575b1ef6fce573c9548823986be76c14ec4
Mateo Balcorta <misterrattail@gmail.com>
2018-09-28 09:25:05 -0700

    Add polyfil to entry point.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 module.exports = [{
-    devServer: {
-        inline:true,
-        port: 8008
-    },
-    entry: "./js/call.jsx",
-    output: {
-        path: __dirname + '/js',
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /(node_modules)/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['latest', 'react']
-            }
-        }]
-    }
+  devServer: {
+    inline: true,
+    port: 8008,
+  },
+  entry: ['@babel/polyfill', ' ./js/call.jsx'],
+  output: {
+    path: `${__dirname  }/js`,
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['latest', 'react'],
+      },
+    }],
+  },
 }];
